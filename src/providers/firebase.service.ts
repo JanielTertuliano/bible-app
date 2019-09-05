d2efed17ac688659dc5ff1a216b912b25a3bebdd
Janiel <janiel.web@gmail.com>
2019-09-05 14:50:21 -0300

    Instal firebase

---
+++ src/providers/firebase.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { FirebaseApp } from '@angular/fire';
+import * as firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  eventEmitter: any = new EventEmitter();
+
+  constructor(
+    private db: AngularFirestore,
+    public firebaseauth: AngularFireAuth,
+    private storage: AngularFireStorage,
+    private fbApp: FirebaseApp
+  ) {
+  }
+
+}
